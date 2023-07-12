504deed017ae425e8f1eb44e026005926d2b58d6
Sasha Abramovich <sshabramovich@Sashas-MacBook-Pro.local>
2023-07-12 23:52:42 +0200

    Add Appolo client and graphQL query for getting top Lens recommendations (WIP)

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,95 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useQuery, gql } from '@apollo/client'
 
 const inter = Inter({ subsets: ['latin'] })
+
+// Load recommended profiles from Lens using GraphQL query
+const recommendedProfiles = gql`
+  query RecommendedProfiles {
+    recommendedProfiles {
+        id
+      name
+      bio
+      attributes {
+        displayType
+        traitType
+        key
+        value
+      }
+        followNftAddress
+      metadata
+      isDefault
+      picture {
+        ... on NftImage {
+          contractAddress
+          tokenId
+          uri
+          verified
+        }
+        ... on MediaSet {
+          original {
+            url
+            mimeType
+          }
+        }
+        __typename
+      }
+      handle
+      coverPicture {
+        ... on NftImage {
+          contractAddress
+          tokenId
+          uri
+          verified
+        }
+        ... on MediaSet {
+          original {
+            url
+            mimeType
+          }
+        }
+        __typename
+      }
+      ownedBy
+      dispatcher {
+        address
+        canUseRelay
+      }
+      stats {
+        totalFollowers
+        totalFollowing
+        totalPosts
+        totalComments
+        totalMirrors
+        totalPublications
+        totalCollects
+      }
+      followModule {
+        ... on FeeFollowModuleSettings {
+          type
+          amount {
+            asset {
+              symbol
+              name
+              decimals
+              address
+            }
+            value
+          }
+          recipient
+        }
+        ... on ProfileFollowModuleSettings {
+        type
+        }
+        ... on RevertFollowModuleSettings {
+        type
+        }
+      }
+    }
+  }
+`;
 
 export default function Home() {
   return (
